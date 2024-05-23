b4f6e6ad3c7ce734450edf2fab525d1a375928dd
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-05-23 17:40:44 -0400

    cors

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ require("dotenv").config();
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdfe45we45w345wegw345werjktjwertkj";
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://mern-stack-blog-portfolio-frontend.onrender.com",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
