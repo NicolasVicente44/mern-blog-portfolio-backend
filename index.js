379ff8c2a6a371e9120c6414698e2e58c634926e
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-05-24 10:39:00 -0400

    cors methods

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true, // Add this line
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify the allowed HTTP methods
   })
 );
 
