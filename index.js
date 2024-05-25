39f93c6276a5b0f7f19f555ed8e79fdb7887ca97
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-05-25 10:49:46 -0400

    allowed origis

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ app.use(
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", true); // Add this line
+
   next();
 });
 app.options("*", cors());
